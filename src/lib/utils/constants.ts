6dc4a878a0b4a4884580f10b558bdba15dc175d4
Sebastian <sebastianwijkhuizen@gmail.com>
2024-06-20 09:27:43 +0200

    feat!: remove constants that are no longer used by other services

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,9 +1,5 @@
 export const REDIS = {
-  KEYS: {
-    blockNumber: "chain:blockNumber",
-    gasPrice: "chain:gasPrice",
-    txjobs: "jobs:tx",
-  } as const,
+  KEYS: { txjobs: "jobs:tx" } as const,
   CHANNELS: {
     blockNumber: "channel:blockNumber",
     gasPrice: "channel:gasPrice",
